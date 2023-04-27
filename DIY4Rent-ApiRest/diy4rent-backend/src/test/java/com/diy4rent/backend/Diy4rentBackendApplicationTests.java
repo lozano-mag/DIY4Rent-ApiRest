18f5a23a6c11647da6b51438be74221f07d311f7
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-28 01:10:44 +0200

    Update Diy4rentBackendApplicationTests.java

--- DIY4Rent-ApiRest/diy4rent-backend/src/test/java/com/diy4rent/backend/Diy4rentBackendApplicationTests.java
+++ DIY4Rent-ApiRest/diy4rent-backend/src/test/java/com/diy4rent/backend/Diy4rentBackendApplicationTests.java
@@ -3,11 +3,32 @@ package com.diy4rent.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.diy4rent.backend.model.Herramienta;
+import com.diy4rent.backend.repository.HerramientaRepository;
+
+import com.diy4rent.backend.model.Usuario;
+import com.diy4rent.backend.repository.UsuarioRepository;
+
 @SpringBootTest
 class Diy4rentBackendApplicationTests {
+	
+    @Autowired
+    private HerramientaRepository herramientaRepository;
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
 	@Test
-	void contextLoads() {
+	void testHerramienta() {
 	}
 
 }
