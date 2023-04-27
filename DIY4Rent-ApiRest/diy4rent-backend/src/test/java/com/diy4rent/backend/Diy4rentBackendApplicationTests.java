f508d5daaef6b183f87350d0ae5b110fc03956d5
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-28 01:37:00 +0200

    Update Diy4rentBackendApplicationTests.java

--- DIY4Rent-ApiRest/diy4rent-backend/src/test/java/com/diy4rent/backend/Diy4rentBackendApplicationTests.java
+++ DIY4Rent-ApiRest/diy4rent-backend/src/test/java/com/diy4rent/backend/Diy4rentBackendApplicationTests.java
@@ -29,6 +29,37 @@ class Diy4rentBackendApplicationTests {
 
 	@Test
 	void testHerramienta() {
+		
+	Herramienta herramienta = new Herramienta();
+	herramienta.setId(1);
+        herramienta.setNombre("Pelacables con ajuste automático Weidmüller");
+        herramienta.setCategoria("Pelacables");
+        herramienta.setEstadoDesgaste(2);
+        herramienta.setUserId(1);
+        herramienta.setAlqAct(0);
+        herramienta.setPrecio(2.5);
+        herramienta.setDescripcion("Con el alicate pelacables automático STRIPAX - 9005000000 de Weidmüller se pueden pelar fácilmente conductores de hilo fino o múltiple con sección desde 0,08 hasta 10 mm² hasta un largo máximo de 25 mm.");
+        herramienta.setFoto("https://media.automation24.com/Artikelbilder/Shop800px/102862_1.jpg");
+
+      
+        herramientaRepository.save(herramienta);
+        
+        Optional<Herramienta> herramienta2 = herramientaRepository.findById((long) 1);
+        assertEquals(herramienta2.get().getNombre(), herramienta.getNombre());
+        assertEquals(herramienta2.get().getNombre(), "Pelacables con ajuste automático Weidmüller");
+        
+        herramienta.setEstadoDesgaste(1);
+        herramientaRepository.save(herramienta);
+        
+        herramienta2 = herramientaRepository.findById((long) 1);
+        assertNotEquals(herramienta2.get().getEstadoDesgaste(), 2);
+        assertEquals(herramienta2.get().getEstadoDesgaste(), 1);
+        
+        
+        herramientaRepository.delete(herramienta);
+        herramienta2 = herramientaRepository.findById((long) 1);
+        assertFalse(herramienta2.isPresent());
+		
 	}
 
 }
